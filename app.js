d2f8ab8b87ae62a3f91aaf33010a84f2140d0717
vamsivsp <39555418+vamsivsp@users.noreply.github.com>
2021-05-30 21:57:09 +0530

    code updated

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>My TVS Express Demo App</h1> <h4>Message: Successfully created my first code deploy with code pipeline </h4> <p>Version 1</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
-      productId: '101',
+      productId: 'Icecreams',
       price: 100
     },
     {
-      productId: '102',
+      productId: 'Milkshakes',
       price: 150
     }
   ])
